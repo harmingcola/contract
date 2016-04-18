5f70c6c4e40fce17ebcf5e49a97d1a74186eeda6
Colin Graham <colin.graham@logentries.com>
2016-04-18 13:02:23 +0100

    Doc update

--- documentation/src/site/sphinx/conf.py
+++ documentation/src/site/sphinx/conf.py
@@ -12,7 +12,7 @@ copyright = 'Seekay'
 html_theme = 'sphinx_rtd_theme'
 html_show_sphinx = False
 
-html_static_path = ['css/custom.css']
+html_static_path = ['css/custom.css', "images"]
 html_context = {
     'css_files': [
         '_static/custom.css',  # overrides for wide tables in RTD theme
