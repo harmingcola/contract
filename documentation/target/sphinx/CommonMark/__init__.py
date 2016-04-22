feef5223601923ae31850d3642956ee756adedaa
seekay-jenkins <thesuperflycrib@gmail.com>
2016-04-22 21:21:59 +0100

    Documentation update

---
+++ documentation/target/sphinx/CommonMark/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, unicode_literals
+from CommonMark.CommonMark import HtmlRenderer
+from CommonMark.CommonMark import Parser
+from CommonMark.CommonMark import dumpAST
+from CommonMark.CommonMark import ASTtoJSON
+from CommonMark.CommonMark import commonmark
+__all__ = ["HtmlRenderer", "Parser", "dumpAST", "ASTtoJSON", "commonmark"]
