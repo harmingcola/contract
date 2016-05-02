e28c700fa0ebb596bb25c63eb7435d91485df2e3
Colin Graham <grahamcolins@gmail.com>
2016-05-02 17:30:03 +0100

    Switched command line usage to apache commons CLI

--- contract-all/src/main/java/org/seekay/contract/all/CommandLineInterface.java
+++ contract-all/src/main/java/org/seekay/contract/all/CommandLineInterface.java
@@ -1,93 +1,102 @@
 package org.seekay.contract.all;
 
-import org.eclipse.jgit.util.StringUtils;
+import org.apache.commons.cli.*;
 import org.seekay.contract.client.client.ContractClient;
 import org.seekay.contract.server.ContractServer;
 
 import static java.lang.Integer.valueOf;
-import static java.lang.System.out;
 import static java.lang.Thread.sleep;
-import static java.util.Arrays.asList;
 
 public class CommandLineInterface {
+	
+	public static void main(String[] args) throws Exception {
 
-  public static void main(String[] args) {
-    checkArgs(args);
-    if(argumentIs("run-server", args, 0) && args.length == 3) {
-      startServerUnsecuredSource(args[1], args[2]);
-      waitUntilKilled();
-    } else if(argumentIs("run-server", args, 0) && args.length == 5) {
-      startServerSecuredSource(args[1], args[2], args[3], args[4]);
-      waitUntilKilled();
-    } else if(argumentIs("run-client", args, 0) && args.length == 3) {
-      startClientUnsecuredSource(args[1], args[2]);
-    } else if(argumentIs("run-client", args, 0) && args.length == 5) {
-      startClientSecuredSource(args[1], args[2], args[3], args[4]);
-    } else {
-      exitWithHelp(args);
-    }
-  }
+		Options options = buildOptions();
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
 
-  private static void startClientSecuredSource(String target, String source, String username, String password) {
-    ContractClient.newClient().withGitConfig(source, username, password).againstPath(target).runTests();
-  }
+		boolean displayHelp = true;
+		if(cmd.hasOption("s")) {
+			if(cmd.hasOption("p") && cmd.hasOption("g") && !cmd.hasOption("u") && !cmd.hasOption("q")) {
+				startServerUnsecuredSource(cmd.getOptionValue("p"), cmd.getOptionValue("g"));
+				displayHelp = false;
+			}
+			else if(cmd.hasOption("p") && cmd.hasOption("g") && cmd.hasOption("u") && cmd.hasOption("q")) {
+				startServerSecuredSource(cmd.getOptionValue("p"), cmd.getOptionValue("g"), cmd.getOptionValue("u"), cmd.getOptionValue("q"));
+				displayHelp = false;
+			}
+		}
+		else if(cmd.hasOption("c")) {
+			if(cmd.hasOption("t") && cmd.hasOption("g") && !cmd.hasOption("u") && !cmd.hasOption("q")) {
+				startClientUnsecuredSource(cmd.getOptionValue("t"), cmd.getOptionValue("g"));
+				displayHelp = false;
+			}
+			else if(cmd.hasOption("t") && cmd.hasOption("g") && cmd.hasOption("u") && cmd.hasOption("q")) {
+				startClientSecuredSource(cmd.getOptionValue("t"), cmd.getOptionValue("g"), cmd.getOptionValue("u"), cmd.getOptionValue("q"));
+				displayHelp = false;
+			}
+		}
 
-  private static void startClientUnsecuredSource(String target, String source) {
-    ContractClient.newClient().withGitConfig(source).againstPath(target).runTests();
-  }
+		if(displayHelp) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("contract", "", options, "", true);
+		}
+	}
 
-  private static void startServerSecuredSource(String port, String source, String username, String password) {
-    ContractServer server = ContractServer.newServer().onPort(valueOf(port));
-    server.withGitConfig(source, username, password);
-    server.startServer();
-  }
+	private static void startClientSecuredSource(String target, String source, String username, String password) {
+		ContractClient.newClient().withGitConfig(source, username, password).againstPath(target).runTests();
+	}
 
-  private static void waitUntilKilled() {
-    while(true) try {
-      sleep(10000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-  }
+	private static void startClientUnsecuredSource(String target, String source) {
+		ContractClient.newClient().withGitConfig(source).againstPath(target).runTests();
+	}
 
-  private static void startServerUnsecuredSource(String port, String source) {
-    ContractServer server = ContractServer.newServer().onPort(valueOf(port));
-    if(source.startsWith("http") || source.startsWith("git")) {
-      server.withGitConfig(source);
-    } else {
-      server.withLocalConfig(source);
-    }
-    server.startServer();
-  }
+	private static void startServerSecuredSource(String port, String source, String username, String password) {
+		ContractServer server = ContractServer.newServer().onPort(valueOf(port));
+		server.withGitConfig(source, username, password);
+		server.startServer();
+		waitUntilKilled();
+	}
 
-  private static boolean argumentIs(String argValue, String[] args, int i) {
-    return args[i].equalsIgnoreCase(argValue);
-  }
+	private static void startServerUnsecuredSource(String port, String source) {
+		ContractServer server = ContractServer.newServer().onPort(valueOf(port));
+		if (source.startsWith("http") || source.startsWith("git")) {
+			server.withGitConfig(source);
+		} else {
+			server.withLocalConfig(source);
+		}
+		server.startServer();
+		waitUntilKilled();
+	}
 
-  private static void checkArgs(String[] args) {
-    if(args.length == 0) {
-      exitWithHelp(args);
-    }
-    if(!args[0].equalsIgnoreCase("run-server") && !args[0].equalsIgnoreCase("run-client")) {
-      exitWithHelp(args);
-    }
-  }
+	private static void waitUntilKilled() {
+		while (true) try {
+			sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-  private static void exitWithHelp(String[] args) {
-    out.println("------------------------------");
-    out.println("--- Contract Command Line");
-    out.println("------------------------------");
-    out.println("--- Invalid parameters passed");
-    out.println("--- ");
-    out.println("--- " + StringUtils.join(asList(args), ", "));
-    out.println("--- ");
-    out.println("--- Valid Command : <run-server> <port> <source> <username> <passwword>");
-    out.println("--- Valid Command : <run-client> <target> <source> <username> <passwword>");
-    out.println("--- Username and password can be omitted for local and unsecured sources");
-    out.println("--- ");
-    out.println("--- Example : run-server 8080 git@github.com:harmingcola/kvServerContracts.git");
-    out.println("--- Example : run-client 8080 git@github.com:harmingcola/kvServerContracts.git");
-    out.println("------------------------------");
-    System.exit(1);
-  }
+	private static Options buildOptions() {
+		Options options = new Options();
+
+		OptionGroup libraryMode = new OptionGroup();
+		libraryMode.addOption(new Option("s", "server", false, "starts the jar in server mode"));
+		libraryMode.addOption(new Option("c", "client", false, "starts the jar in client mode"));
+		options.addOptionGroup(libraryMode);
+
+		OptionGroup modeOptions = new OptionGroup();
+		modeOptions.addOption(new Option("p", "port", true, "Port to start server on, only needed when running server"));
+		modeOptions.addOption(new Option("t", "target", true, "Target server for tests, only needed when running client"));
+		options.addOptionGroup(modeOptions);
+
+		options.addOption("g", "source", true, "URL of the git repository or local location contracts should be loaded from");
+		options.addOption("u", "username", true, "Username of secured git repository, optional");
+		options.addOption("q", "password",true,"Password of secured git repository, optional");
+		options.addOption("h", "help",false,"Prints usage info");
+
+		return options;
+	}
+
 }
+
