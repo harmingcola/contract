8f6b1fda459a205653d6c22f1488be3f34c290ef
Colin Graham <colin.graham@logentries.com>
2016-05-03 16:54:21 +0100

    Git is being a dick

--- contract-common/src/test/groovy/org/seekay/contract/common/match/MatchingServiceSpec.groovy
+++ contract-common/src/test/groovy/org/seekay/contract/common/match/MatchingServiceSpec.groovy
@@ -40,66 +40,66 @@ class MatchingServiceSpec extends Specification{
 
     def "a contract matching all parameters should return correctly"() {
         given:
-            Contract contract = ContractTestFixtures.defaultGetContract().build()
-            methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
-            pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
-            headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
+        Contract contract = ContractTestFixtures.defaultGetContract().build()
+        methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
+        pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
+        headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
         when:
-            Contract matchedContract = service.matchGetRequest(contract.request)
+        Contract matchedContract = service.matchGetRequest(contract.request)
         then:
-            matchedContract.response.status == contract.response.status
-            matchedContract.response.body == contract.response.body
-            matchedContract.response.headers == contract.response.headers
+        matchedContract.response.status == contract.response.status
+        matchedContract.response.body == contract.response.body
+        matchedContract.response.headers == contract.response.headers
     }
 
     def "a contract not matching method should return null"() {
         given:
-            Contract contract = ContractTestFixtures.defaultGetContract().build()
-            methodMatcher.findMatches(_ as Set<Contract>, GET) >> {EMPTY_SET}
-            pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
-            headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
-            bodyMatchService.findMatches(_ as Set<Contract>,_ as ContractRequest) >> {[contract]}
+        Contract contract = ContractTestFixtures.defaultGetContract().build()
+        methodMatcher.findMatches(_ as Set<Contract>, GET) >> {EMPTY_SET}
+        pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
+        headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
+        bodyMatchService.findMatches(_ as Set<Contract>,_ as ContractRequest) >> {[contract]}
         when:
-            Contract matchedContract = service.matchPostRequest(contract.request)
+        Contract matchedContract = service.matchPostRequest(contract.request)
         then:
-            matchedContract == null
+        matchedContract == null
     }
 
     def "a contract not matching the path should return null"() {
         given:
-            Contract contract = ContractTestFixtures.defaultGetContract().build()
-            methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
-            pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {EMPTY_SET}
-            headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
-            bodyMatchService.findMatches(_ as Set<Contract>,_ as ContractRequest) >> {[contract]}
+        Contract contract = ContractTestFixtures.defaultGetContract().build()
+        methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
+        pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {EMPTY_SET}
+        headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract]}
+        bodyMatchService.findMatches(_ as Set<Contract>,_ as ContractRequest) >> {[contract]}
         when:
-            Contract matchedContract = service.matchPutRequest(contract.request)
+        Contract matchedContract = service.matchPutRequest(contract.request)
         then:
-            matchedContract == null
+        matchedContract == null
     }
 
     def "a contract not matching headers should return null"() {
         given:
-            Contract contract = ContractTestFixtures.defaultGetContract().build()
-            methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
-            pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
-            headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {EMPTY_SET}
+        Contract contract = ContractTestFixtures.defaultGetContract().build()
+        methodMatcher.findMatches(_ as Set<Contract>, GET) >> {[contract]}
+        pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract]}
+        headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {EMPTY_SET}
         when:
-            Contract matchedContract = service.matchDeleteRequest(contract.request)
+        Contract matchedContract = service.matchDeleteRequest(contract.request)
         then:
-            matchedContract == null
+        matchedContract == null
     }
 
     def "if multiple contracts are matched, an illegal state should be thrown" () {
         given:
-            Contract contract1 = ContractTestFixtures.defaultPostContract().build()
-            Contract contract2 = ContractTestFixtures.defaultGetContract().build()
-            methodMatcher.findMatches(_ as Set<Contract>, POST) >> {[contract1, contract2]}
-            pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract1, contract2]}
-            headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract1, contract2]}
+        Contract contract1 = ContractTestFixtures.defaultPostContract().build()
+        Contract contract2 = ContractTestFixtures.defaultGetContract().build()
+        methodMatcher.findMatches(_ as Set<Contract>, POST) >> {[contract1, contract2]}
+        pathMatchingService.findMatches(_ as Set<Contract>,_ as String) >> {[contract1, contract2]}
+        headerMatcher.isMatch(_ as Set<Contract>,_ as Map<String, String>) >> {[contract1, contract2]}
         when:
-            service.matchGetRequest(contract1.request)
+        service.matchGetRequest(contract1.request)
         then:
-            thrown IllegalStateException
+        thrown IllegalStateException
     }
 }
--- contract-model/src/main/java/org/seekay/contract/model/tools/SetTools.java
+++ contract-model/src/main/java/org/seekay/contract/model/tools/SetTools.java
@@ -5,129 +5,129 @@ import java.util.*;
 
 public class SetTools {
 
-  /**
-   * Private constructor for utility class
-   */
-  private SetTools() {
-    throw new IllegalStateException("Utility classes should never be constructed");
-  }
-
-  /**
-   * For multiple sets, only the elements contained in all will be returned
-   *
-   * @param sets
-   * @param <T>
-   * @return
-   */
-  public static <T> Set<T> intersectingElements(Set<T>... sets) {
-    assert sets.length > 1;
-
-    Set<T> result = new HashSet<T>();
-
-    Set<T> head = head(sets);
-    List<Set<T>> tail = tail(sets);
-
-    for (T t : head) {
-      if (containedInAll(t, tail)) {
-        result.add(t);
-      }
+    /**
+     * Private constructor for utility class
+     */
+    private SetTools() {
+        throw new IllegalStateException("Utility classes should never be constructed");
     }
 
-    return result;
-  }
+    /**
+     * For multiple sets, only the elements contained in all will be returned
+     *
+     * @param sets
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> intersectingElements(Set<T>... sets) {
+        assert sets.length > 1;
 
-  /**
-   * Returns the first element in a set
-   *
-   * @param set
-   * @param <T>
-   * @return
-   */
-  public static <T> T head(Set<T> set) {
-    return set.iterator().next();
-  }
+        Set<T> result = new HashSet<T>();
 
-  /**
-   * Returns the first set in a vararg of sets
-   *
-   * @param sets
-   * @param <T>
-   * @return
-   */
-  public static <T> Set<T> head(Set<T>... sets) {
-    return Arrays.asList(sets).get(0);
-  }
+        Set<T> head = head(sets);
+        List<Set<T>> tail = tail(sets);
 
-  /**
-   * Returns all but the first element of a set
-   *
-   * @param set
-   * @param <T>
-   * @return
-   */
-  public static <T> Set<T> tail(Set<T> set) {
-    Set<T> result = new HashSet<T>();
-    Iterator<T> iterator = set.iterator();
-    iterator.next();
-    while (iterator.hasNext()) {
-      result.add(iterator.next());
+        for (T t : head) {
+            if (containedInAll(t, tail)) {
+                result.add(t);
+            }
+        }
+
+        return result;
     }
 
-    return result;
-  }
+    /**
+     * Returns the first element in a set
+     *
+     * @param set
+     * @param <T>
+     * @return
+     */
+    public static <T> T head(Set<T> set) {
+        return set.iterator().next();
+    }
 
-  /**
-   * Converts a set of strings to an array of strings
-   *
-   * @param input
-   * @return
-   */
-  public static String[] toArray(Set<String> input) {
-    if(input == null) {
-      return new String[0];
+    /**
+     * Returns the first set in a vararg of sets
+     *
+     * @param sets
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> head(Set<T>... sets) {
+        return Arrays.asList(sets).get(0);
     }
-    String[] output = new String[input.size()];
-    int i = 0;
-    for (String inputString : input) {
-      output[i] = inputString;
-      i++;
-    }
-    return output;
-  }
 
-  /**
-   * Converts an array of strings to a set of strings
-   * @param input
-   * @return
-   */
-  public static Set<String> toSet(String[] input) {
-    Set<String> output = new LinkedHashSet<String>();
-    if(input == null) {
-      return output;
-    }
-    for(String inputString : input) {
-      output.add(inputString);
-    }
-    return output;
-  }
+    /**
+     * Returns all but the first element of a set
+     *
+     * @param set
+     * @param <T>
+     * @return
+     */
+    public static <T> Set<T> tail(Set<T> set) {
+        Set<T> result = new HashSet<T>();
+        Iterator<T> iterator = set.iterator();
+        iterator.next();
+        while (iterator.hasNext()) {
+            result.add(iterator.next());
+        }
 
-  private static <T> List<Set<T>> tail(Set<T>... sets) {
-    if (sets.length < 2) {
-      throw new IllegalStateException("Matching configured incorrectly");
+        return result;
     }
-    List<Set<T>> lists = Arrays.asList(sets);
-    return lists.subList(1, lists.size());
-  }
 
-  private static <T> boolean containedInAll(T t, List<Set<T>> sets) {
-    boolean found = true;
-    for (Set<T> set : sets) {
-      if (!set.contains(t)) {
-        found = false;
-      }
+    /**
+     * Converts a set of strings to an array of strings
+     *
+     * @param input
+     * @return
+     */
+    public static String[] toArray(Set<String> input) {
+        if(input == null) {
+            return new String[0];
+        }
+        String[] output = new String[input.size()];
+        int i = 0;
+        for (String inputString : input) {
+            output[i] = inputString;
+            i++;
+        }
+        return output;
     }
-    return found;
-  }
+
+    /**
+     * Converts an array of strings to a set of strings
+     * @param input
+     * @return
+     */
+    public static Set<String> toSet(String[] input) {
+        Set<String> output = new LinkedHashSet<String>();
+        if(input == null) {
+            return output;
+        }
+        for(String inputString : input) {
+            output.add(inputString);
+        }
+        return output;
+    }
+
+    private static <T> List<Set<T>> tail(Set<T>... sets) {
+        if (sets.length < 2) {
+            throw new IllegalStateException("Matching configured incorrectly");
+        }
+        List<Set<T>> lists = Arrays.asList(sets);
+        return lists.subList(1, lists.size());
+    }
+
+    private static <T> boolean containedInAll(T t, List<Set<T>> sets) {
+        boolean found = true;
+        for (Set<T> set : sets) {
+            if (!set.contains(t)) {
+                found = false;
+            }
+        }
+        return found;
+    }
 
 
 }
